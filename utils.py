73ba3cc3b4cf255bd772ba89983096db3098a005
Jonas <frankemoelle.jonas@gmail.com>
2025-06-25 19:13:17 +0000

    move functions to utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,17 @@
 import json
+import collections
 
-from transformers import AutoTokenizer
+import torch
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from rouge_score import rouge_scorer
+from peft import get_peft_model, get_peft_model_state_dict, set_peft_model_state_dict, LoraConfig, TaskType
 
+
+from fedn.utils.helpers.helpers import get_helper
+
+HELPER_MODULE = "numpyhelper"
+helper = get_helper(HELPER_MODULE)
 
 model = "HuggingFaceTB/SmolLM2-135M"
 tokenizer = AutoTokenizer.from_pretrained(model)
@@ -48,3 +57,37 @@ def compute_rouge(predictions, references):
         'rougeL': sum(s['rougeL'].fmeasure for s in scores) / len(scores),
     }
     return avg_scores
+
+
+def compile_model():
+    model_name = "HuggingFaceTB/SmolLM2-135M"
+
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+
+    model.config.pad_token_id = model.config.eos_token_id
+
+    lora_config = LoraConfig(
+        r=8,
+        lora_alpha=16,
+        target_modules=["q_proj", "v_proj"],
+        lora_dropout=0.1,
+        task_type=TaskType.CAUSAL_LM,
+    )
+    peft_model = get_peft_model(model, lora_config)
+    return peft_model
+
+
+def load_lora_parameters(model_path):
+    """Loads the LoRA adaperts, not the full model"""
+    peft_model = compile_model()
+    parameters_np = helper.load(model_path)
+
+    peft_model_statedict = get_peft_model_state_dict(peft_model)
+    params_dict = zip(peft_model_statedict.keys(), parameters_np)
+    lora_state_dict = collections.OrderedDict(
+        {key: torch.tensor(x) for key, x in params_dict}
+    )
+
+    set_peft_model_state_dict(peft_model, lora_state_dict)
+
+    return peft_model
